a854b0ccdd1fe48fbe00b8f5f1f21d96761fc887
Johnson Jiang <lizwjiang@gmail.com>
2015-06-18 13:48:03 +0800

    remove the reader from LogHandler

--- laas-core/src/main/java/org/g6/laas/core/log/LogAnalyzer.java
+++ laas-core/src/main/java/org/g6/laas/core/log/LogAnalyzer.java
@@ -3,7 +3,7 @@ package org.g6.laas.core.log;
 import org.g6.laas.core.file.ILogFile;
 import org.g6.laas.core.file.LogFile;
 
-public class LogController {
+public class LogAnalyzer {
     public void analyse(){
         String fileName = "aa.txt";
         ILogFile file = new LogFile(fileName);
--- laas-core/src/main/java/org/g6/laas/core/log/LogHandler.java
+++ laas-core/src/main/java/org/g6/laas/core/log/LogHandler.java
@@ -1,6 +1,7 @@
 package org.g6.laas.core.log;
 
 import org.g6.laas.core.file.ILogFile;
+import org.g6.laas.core.rule.Rule;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,14 +10,17 @@ import java.util.List;
 
 public abstract class LogHandler{
     List<ILogFile> list;
+    Rule  rule;
 
-    public LogHandler(ILogFile iLogFile) {
+    public LogHandler(ILogFile file, Rule rule) {
         list = new ArrayList<ILogFile>();
-        list.add(iLogFile);
+        list.add(file);
+        this.rule = rule;
     }
 
-    public LogHandler(List<ILogFile> list){
+    public LogHandler(List<ILogFile> list, Rule rule){
         this.list = list;
+        this.rule = rule;
     }
 
     public abstract Iterator<? extends Slice> handle() throws IOException;
