a6cc2d05c00557b1c6319f2f67c4e2d8aa5a69fe
Johnson <li.jiang3@hp.com>
2015-06-28 22:56:11 +0800

    add json file to store the line formats for SM RTE log file and modify SM RTE format

--- laas-sm/src/main/java/org/g6/laas/sm/format/SMRTELogInputFormat.java
+++ laas-sm/src/main/java/org/g6/laas/sm/format/SMRTELogInputFormat.java
@@ -1,4 +1,4 @@
-package org.g6.laas.sm;
+package org.g6.laas.sm.format;
 
 import org.g6.laas.core.format.FieldFormat;
 import org.g6.laas.core.format.InputFormat;
@@ -34,8 +34,13 @@ public class SMRTELogInputFormat implements InputFormat {
     }
 
     @Override
-    public Map<String, List<FieldFormat>> getFormats() {
+    public Map<String, List<FieldFormat>> getLineFormatsByKey() {
          //TODO the keyword parts will be from property file or database. now it can be hardcoded.
+        return null;
+    }
+
+    @Override
+    public Map<String, String> getRegex4LineSplit() {
         return null;
     }
 }
