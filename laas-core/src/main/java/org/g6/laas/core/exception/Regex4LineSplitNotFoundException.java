4e41beaac9c543de7bb7d65eaab7e365c0ce8b2d
Johnson <li.jiang3@hp.com>
2015-06-28 22:53:02 +0800

    Add Exception for regex not found

---
+++ laas-core/src/main/java/org/g6/laas/core/exception/Regex4LineSplitNotFoundException.java
@@ -0,0 +1,11 @@
+package org.g6.laas.core.exception;
+
+public class Regex4LineSplitNotFoundException extends LaaSRuntimeException {
+    public Regex4LineSplitNotFoundException() {
+        super();
+    }
+
+    public Regex4LineSplitNotFoundException(String message) {
+        super(message);
+    }
+}
