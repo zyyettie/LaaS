7238bc69a2267576369bb677edcc1dbfdeaf85a5
Johnson <li.jiang3@hp.com>
2015-06-29 23:17:21 +0800

    remove the calling on split in the constructor of LogLine

--- laas-core/src/main/java/org/g6/laas/core/log/LogLine.java
+++ laas-core/src/main/java/org/g6/laas/core/log/LogLine.java
@@ -17,8 +17,6 @@ import java.util.*;
 
 @NoArgsConstructor
 public class LogLine extends Line {
-    Object sortValue;
-
     Field sortedField = null;
 
     public LogLine(ILogFile file, String content, int lineNumber) {
@@ -27,16 +25,9 @@ public class LogLine extends Line {
 
     public LogLine(ILogFile file, String content, int lineNumber, InputFormat lineFormat) {
         super(file, content, lineNumber, lineFormat);
-        split();
     }
 
     /**
-     * Whether split depends on the search result. From the code design, if InputFormat comes
-     * we think each line in the return result should be split according to matching format.
-     * <p/>
-     * But there are some limitations. one is all lines must have matching formats, otherwise
-     * the InputFormatNotFoundException is thrown.
-     * <p/>
      * The specified InputFormat can not cover all the ones. so the split method can be overwritten
      * and no matter whether the InputFormat is needed or not, because the code will run on the line
      * content directly.
@@ -45,86 +36,73 @@ public class LogLine extends Line {
      */
     @Override
     public Collection<Field> split() {
-        if (isSplitNeeded()) {
-            String lineSplitRegex = null;
-            List<FieldFormat> fieldFormatList = null;
-            List<String> errorKeyList = new ArrayList<>();
-            int counter = 0;
-            //here if only split with separator, the line format should not be special
-            Map<String, List<FieldFormat>> formats = getInputFormat().getLineFormatsByKey();
-            for (Map.Entry<String, List<FieldFormat>> entry : formats.entrySet()) {
-                if (getContent().contains(entry.getKey())) {
-                    errorKeyList.add(entry.getKey());
-                    counter++;
-                    fieldFormatList = entry.getValue();
-                    lineSplitRegex = getInputFormat().getRegex4LineSplit().get(entry.getKey());
-                }
+        String lineSplitRegex = null;
+        List<FieldFormat> fieldFormatList = null;
+        List<String> errorKeyList = new ArrayList<>();
+        int counter = 0;
+
+        Map<String, List<FieldFormat>> formats = getInputFormat().getLineFormatsByKey();
+        for (Map.Entry<String, List<FieldFormat>> entry : formats.entrySet()) {
+            if (getContent().contains(entry.getKey())) {
+                errorKeyList.add(entry.getKey());
+                counter++;
+                fieldFormatList = entry.getValue();
+                lineSplitRegex = getInputFormat().getRegex4LineSplit().get(entry.getKey());
             }
-            if (counter != 1) {
-                StringBuffer sb = new StringBuffer();
-                sb.append("Found more than one formats for the line which number is ")
-                        .append(getLineNumber())
-                        .append(" in ")
-                        .append(getFile().getName())
-                        .append(". The key words matched are: ");
-                for (String key : errorKeyList) {
-                    sb.append(key)
-                            .append(", ");
-                }
-                //TODO remove the last comma
-                throw new LaaSRuntimeException(sb.toString());
-            }
-
-            if (fieldFormatList == null)
-                throw new InputFormatNotFoundException("InputFormat not found");
-            if (lineSplitRegex == null)
-                throw new Regex4LineSplitNotFoundException("Regex not found for " + getContent());
-
-            String[] fieldContents = RegexUtil.getValues(getContent(), lineSplitRegex);
-            Collection<Field> fieldList = new ArrayList<>();
-
-            for (int i = 0; i < fieldContents.length; i++) {
-                Field f = null;
-                FieldFormat ff = fieldFormatList.get(i);
-                String fieldFormatType = ff.getType();
-
-                if (fieldFormatType.equals("String")) {
-                    f = new TextField(fieldContents[i]);
-                    if (ff.isSortable()) {
-                        this.sortValue = fieldContents[i];
-                    }
-                } else if (fieldFormatType.equals("Integer")) {
-                    f = new IntegerField(fieldContents[i]);
-                } else if (fieldFormatType.equals("DateTime")) {
-                  //DODO
-                } else if (fieldFormatType.equals("Double")) {
-                    f = new DoubleField(fieldContents[i]);
-                }
-                fieldList.add(f);
-            }
-            return fieldList;
         }
 
-        return null;
+        if (counter != 1) {
+            StringBuffer sb = new StringBuffer();
+            sb.append("Found more than one formats for the line which number is ")
+                    .append(getLineNumber())
+                    .append(" in ")
+                    .append(getFile().getName())
+                    .append(". The key words matched are: ");
+            int keyCount = 1;
+            for (String key : errorKeyList) {
+                sb.append(key);
+                if (errorKeyList.size() != keyCount) {
+                    sb.append(", ");
+                }
+                keyCount ++;
+            }
+            throw new LaaSRuntimeException(sb.toString());
+        }
+
+        if (fieldFormatList == null)
+            throw new InputFormatNotFoundException("InputFormat not found");
+        if (lineSplitRegex == null)
+            throw new Regex4LineSplitNotFoundException("Regex not found for " + getContent());
+
+        String[] fieldContents = RegexUtil.getValues(getContent(), lineSplitRegex);
+        Collection<Field> fieldList = new ArrayList<>();
+
+        for (int i = 0; i < fieldContents.length; i++) {
+            Field f = null;
+            FieldFormat ff = fieldFormatList.get(i);
+            String fieldFormatType = ff.getType();
+
+            if (fieldFormatType.equals("String")) {
+                f = new TextField(fieldContents[i]);
+            } else if (fieldFormatType.equals("Integer")) {
+                f = new IntegerField(fieldContents[i]);
+            } else if (fieldFormatType.equals("DateTime")) {
+                //DODO
+            } else if (fieldFormatType.equals("Double")) {
+                f = new DoubleField(fieldContents[i]);
+            }
+
+            if (ff.isSortable()) {
+                sortedField = f;
+            }
+            fieldList.add(f);
+        }
+        return fieldList;
     }
-
-    /**
-     * Here should be a limitation that only Double and Date type
-     * can be compared. in split method, no matter what field format type should be
-     * sortValue always must be either Double and Date.
-     *
-     * @param o
-     * @return
-     */
-
 
     @Override
     public int compareTo(Object o) {
-        return sortedField.compareTo(((LogLine)o).sortedField);
-    }
-
-    public Object getSortValue() {
-        return sortValue;
+        return sortedField.compareTo(((LogLine) o).sortedField);
     }
 
 }
