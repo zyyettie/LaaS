eac1b0dc1902ed26194c2fde61bd7ea0a828264d
Perry Wang <perrywangsh@gmail.com>
2015-07-01 15:57:09 +0800

    remove setContext from Analysis interface

--- laas-core/src/main/java/org/g6/laas/core/engine/task/AnalysisTask.java
+++ laas-core/src/main/java/org/g6/laas/core/engine/task/AnalysisTask.java
@@ -1,9 +1,7 @@
 package org.g6.laas.core.engine.task;
 
-import org.g6.laas.core.engine.context.AnalysisContext;
-
 public interface AnalysisTask<T> {
+
   T analyze();
 
-  void setContext(AnalysisContext context);
 }
