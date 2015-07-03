2baf47cebd2c0beaac4ff8a6142e7897b9ddbfbd
Johnson Jiang <lizwjiang@gmail.com>
2015-07-03 13:55:10 +0800

    improve JSON support for InputFormat

--- laas-core/src/main/java/org/g6/laas/core/format/InputFormat.java
+++ laas-core/src/main/java/org/g6/laas/core/format/InputFormat.java
@@ -5,15 +5,9 @@ import java.util.Map;
 
 public interface InputFormat {
     boolean isDiff();
-    void setDiff(boolean diff);
     boolean isDefaultFormat();
-    void setDefaultFormat(boolean df);
     String[] getDefaultFormats() throws IllegalAccessException;
-    void setDefaultFormats(String[] defaultFormats);
     String[] getFormats() throws IllegalAccessException;
-    void setFormats(String[] formats);
     Map<String, List<FieldFormat>> getLineFormatsByKey();
-    void setLineFormatsByKey(Map<String, List<FieldFormat>> lineFormats);
     Map<String, String> getRegex4LineSplit();
-    void setRegex4LineSplit(Map<String, String> regex);
 }
---
+++ laas-core/src/main/java/org/g6/laas/core/format/json/FormatProvider.java
@@ -0,0 +1,19 @@
+package org.g6.laas.core.format.json;
+
+import lombok.AllArgsConstructor;
+import org.g6.laas.core.format.InputFormat;
+import org.g6.util.FileUtil;
+
+import java.util.List;
+
+@AllArgsConstructor
+public abstract class FormatProvider {
+    private String file;
+
+    InputFormat getInputFormat() {
+        List<String> list = FileUtil.readFile(FileUtil.getRelativeInputStream(file));
+        return parse(list);
+    }
+
+    abstract  InputFormat parse(List<String> lineList);
+}
