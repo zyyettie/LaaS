9ff3e67f75efce1112f168b52d8dc4f5a6fc20c4
Johnson <li.jiang3@hp.com>
2015-07-06 21:02:00 +0800

    Add regex support in Rule

---
+++ laas-core/src/main/java/org/g6/laas/core/rule/RegexRule.java
@@ -0,0 +1,18 @@
+package org.g6.laas.core.rule;
+
+import lombok.Data;
+import org.g6.util.RegexUtil;
+
+@Data
+public class RegexRule extends AbstractRule {
+    private String regex;
+
+    public RegexRule(String regex) {
+        this.regex = regex;
+    }
+
+    @Override
+    public boolean isSatisfied(Object content) {
+        return RegexUtil.isMatched(content.toString(),regex);
+    }
+}
