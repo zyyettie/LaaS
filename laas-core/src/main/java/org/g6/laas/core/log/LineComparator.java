10c1d259a8cb33f77cb2bc4eae0bbd7c01ceaba1
Johnson Jiang <lizwjiang@gmail.com>
2015-07-06 12:16:33 +0800

    add LineComparator to support order by desc

---
+++ laas-core/src/main/java/org/g6/laas/core/log/LineComparator.java
@@ -0,0 +1,21 @@
+package org.g6.laas.core.log;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Comparator;
+
+@Data
+@NoArgsConstructor
+public class LineComparator implements Comparator<Line> {
+    boolean desc = true;
+
+    public LineComparator(boolean desc){
+         this.desc = desc;
+    }
+
+    @Override
+    public int compare(Line o1, Line o2) {
+        return desc ? (-1) * o1.compareTo(o2) : o1.compareTo(o2);
+    }
+}
