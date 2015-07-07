7d6e143bc83c53578d9573ef764c2d94192204fa
Johnson Jiang <lizwjiang@gmail.com>
2015-07-07 17:15:04 +0800

    add laas-apache module in pom.xml

--- laas-sm/src/test/java/DemoTest.java
+++ laas-sm/src/test/java/DemoTest.java
@@ -1,47 +1,32 @@
 import org.g6.laas.core.engine.StrategyAnalysisEngine;
 import org.g6.laas.core.engine.ThreadPoolExecutionStrategy;
-import org.g6.laas.core.engine.context.SimpleAnalysisContext;
-import org.g6.laas.core.file.ILogFile;
-import org.g6.laas.core.file.LogFile;
-import org.g6.laas.core.format.json.FormatProvider;
-import org.g6.laas.core.format.json.JSONFormatProvider;
-import org.g6.laas.core.log.BasicLogHandler;
 import org.g6.laas.core.log.Line;
-import org.g6.laas.core.log.LogHandler;
-import org.g6.laas.core.rule.KeywordRule;
-import org.g6.laas.core.rule.Rule;
 import org.g6.laas.sm.task.TopNQueryTask;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class DemoTest {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ILogFile file = new LogFile("e:\\sm.log");
-        Rule rule = new KeywordRule("RTE D DBQUERY");
+    public static void main(String[] args)  {
 
-        LogHandler handler = new BasicLogHandler(file, rule);
-        FormatProvider provider = new JSONFormatProvider("/sm_rte_log.json");
-
-        SimpleAnalysisContext context = new SimpleAnalysisContext();
-        context.getRules().add(rule);
-        context.setHandler(handler);
-        context.setInputForm(provider.getInputFormat());
-
-        TopNQueryTask task = new TopNQueryTask(context);
-
+        TopNQueryTask task = new TopNQueryTask(50, "e:\\sm.log");
 
         StrategyAnalysisEngine engine = new StrategyAnalysisEngine();
         engine.setStrategy(new ThreadPoolExecutionStrategy());
 
-        Future<Collection<Line>> future = engine.submit(task);
+        Future<List<Line>> future = engine.submit(task);
         engine.shutdown();
 
-        List<Line> lines = (List<Line>)future.get();
+        try {
+            List<Line> lines = future.get();
+            System.out.println();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        //FileUtil.writeFile(lines, "");
     }
 }
