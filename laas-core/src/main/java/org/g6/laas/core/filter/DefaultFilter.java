47641287434cd7abc0540761d9b095f4850eb136
zhouanqing <zhouanqing@hotmail.com>
2015-07-09 13:13:23 +0800

    Update DefaultFilter

--- laas-core/src/main/java/org/g6/laas/core/filter/DefaultFilter.java
+++ laas-core/src/main/java/org/g6/laas/core/filter/DefaultFilter.java
@@ -4,4 +4,16 @@ public class DefaultFilter extends AbstractRuleFilter {
     public boolean isFiltered(Object content) {
         return false;
     }
+
+    public IFilter and(IFilter filter) {
+        return this;
+    }
+
+    public IFilter or(IFilter filter) {
+        return this;
+    }
+
+    public IFilter not() {
+        return this;
+    }
 }
