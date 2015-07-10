ac5ec06afb55ace9168eb109cdb69098eac7ac24
zhouanqing <zhouanqing@hotmail.com>
2015-07-10 17:03:50 +0800

    Update Regex for login

--- laas-core/src/main/java/org/g6/util/RegexTest.java
+++ laas-core/src/main/java/org/g6/util/RegexTest.java
@@ -9,14 +9,16 @@ public class RegexTest {
             " 4536( 12784) 06/15/2015 04:43:30  RTE D SCRIPTTRACE: ProcessDesignerEnablement.isPDEnabled entered, line 107",
             " 4536( 12784) 06/15/2015 04:43:30  RTE D SCRIPTTRACE: ProcessDesignerEnablement.isPDEnabled exited, elapsed: 0 ms",
             " 7076(  6028) 05/11/2015 16:49:45  RTE D DBQUERY^F^Todo(sqlserver I)^0^0.000000^P^262^2.921000^\"(itemType=\"incidents\" and status~=\"Resolved\")\"^{\"itemType\"}^0.000000^0.000000 ( [ 0] display fdisp.1 )",
-            " 7076(  6028) 05/11/2015 16:49:32  RTE D DBFIND^F^operator(sqlserver I)^1^0.000000^ ^1^0.078000^\"name=\"WRZJRO\"\"^ ^0.000000^0.000000 ( [ 0] login call.user.login )"
+            " 7076(  6028) 05/11/2015 16:49:32  RTE D DBFIND^F^operator(sqlserver I)^1^0.000000^ ^1^0.078000^\"name=\"WRZJRO\"\"^ ^0.000000^0.000000 ( [ 0] login call.user.login )",
+            " 9584(  9124) 06/20/2015 17:02:17  RTE D Response-Total: 0.328 -- RAD:0.109  JS:0.172 Log:0.047 Database:0.047  (CPU 0.405) Transaction - format:sc.manage.ToDo.g application:display,fdisp.1 option:0"
     };
 
     public static String[] regexes = {
-            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D.+DBACCESS.+(\\d+\\.\\d+)\\s+seconds.*",
+            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D.+DBACCESS.+(\\d+\\.\\d+)\\s+seconds",
             "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D SCRIPTTRACE:.+elapsed: (\\d+) ms",
-            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D DBQUERY(?:\\^[^\\^]+){6}\\^(\\d+\\.\\d+).*",
-            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D DBFIND(?:\\^[^\\^]+){6}\\^(\\d+\\.\\d+).*"
+            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D DBQUERY(?:\\^[^\\^]+){6}\\^(\\d+\\.\\d+)",
+            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D DBFIND(?:\\^[^\\^]+){6}\\^(\\d+\\.\\d+)",
+            "^\\s*(\\d+)\\(\\s+(\\d+)\\)\\s+(\\d+/\\d+/\\d+\\s+\\d+:\\d+:\\d+)\\s+RTE D Response-Total:\\s+(\\d+\\.\\d+).+RAD:(\\d+\\.\\d+)\\s+JS:(\\d+\\.\\d+)\\s+Log:(\\d+\\.\\d+)\\s+Database:(\\d+\\.\\d+)\\s+\\(CPU\\s+(\\d+\\.\\d+)\\).+format:sc\\.manage\\.ToDo\\.g application:display,fdisp\\.1"
     };
 
     public static void main(String[] args) {
@@ -25,7 +27,7 @@ public class RegexTest {
             Pattern p = Pattern.compile(regex);
             for (String sample : samples) {
                 Matcher m = p.matcher(sample);
-                boolean b = m.matches();
+                boolean b = m.find();
                 System.out.println(b);
                 if (b) {
                     int count = m.groupCount();
