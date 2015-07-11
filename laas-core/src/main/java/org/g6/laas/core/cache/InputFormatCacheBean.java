d3fba197550aecb7261ba96b2d4f00eb03fbe8a8
Johnson <li.jiang3@hp.com>
2015-07-11 16:17:57 +0800

    add this to initialize the cache for input format.

---
+++ laas-core/src/main/java/org/g6/laas/core/cache/InputFormatCacheBean.java
@@ -0,0 +1,17 @@
+package org.g6.laas.core.cache;
+
+import org.g6.laas.core.format.cache.InputFormatCache;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InputFormatCacheBean implements InitializingBean {
+    @Autowired
+    private InputFormatCache cache;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        cache.getAllInputFormats();
+    }
+}
