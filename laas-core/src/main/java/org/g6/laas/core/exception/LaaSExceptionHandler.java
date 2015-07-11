8b947e832db2e18ad60480754ada3aa701371d63
Johnson <li.jiang3@hp.com>
2015-07-11 16:16:45 +0800

    refactor LaaSExceptionHandler

--- laas-core/src/main/java/org/g6/laas/core/exception/LaaSExceptionHandler.java
+++ laas-core/src/main/java/org/g6/laas/core/exception/LaaSExceptionHandler.java
@@ -4,8 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LaaSExceptionHandler {
+
+    public static void handleException(String msg) {
+        log.error(msg);
+        throw new LaaSRuntimeException(msg);
+    }
+
     public static void handleException(String msg, Exception e) {
+        handleException(msg, e, true);
+    }
+
+    public static void handleException(String msg, Exception e, boolean isThrown) {
         log.error(msg, e);
-        throw new LaaSRuntimeException(msg, e);
+        if (isThrown)
+            throw new LaaSRuntimeException(msg, e);
     }
 }
