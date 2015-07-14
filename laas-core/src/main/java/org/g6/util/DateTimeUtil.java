8ec1700780a4b4c18e1c2c2ebb27c5ebd4c76072
Pitt Zhou <pitt.zhou2012@gmail.com>
2015-07-15 00:03:39 +0800

    Added utility implementation on date time

---
+++ laas-core/src/main/java/org/g6/util/DateTimeUtil.java
@@ -0,0 +1,72 @@
+package org.g6.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class DateTimeUtil {
+
+    public long getMsecDiff(String string1, String string2, String pattern) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat(pattern);
+        long d1=formatter.parse(string1).getTime();
+        long d2=formatter.parse(string2).getTime();
+        return d1-d2;
+    }
+
+    public long getSecondDiff(String string1, String string2, String pattern) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat(pattern);
+        long d1=formatter.parse(string1).getTime();
+        long d2=formatter.parse(string2).getTime();
+        return (d1-d2)/(1000);
+    }
+
+    public long getMinuteDiff(String string1, String string2, String pattern) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat(pattern);
+        long d1=formatter.parse(string1).getTime();
+        long d2=formatter.parse(string2).getTime();
+        return (d1-d2)/(1000*60);
+    }
+
+    public long getHourDiff(String string1, String string2, String pattern) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat(pattern);
+        long d1=formatter.parse(string1).getTime();
+        long d2=formatter.parse(string2).getTime();
+        return (d1-d2)/(1000*60*60);
+    }
+
+    public long getDayDiff(String string1, String string2, String pattern) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat(pattern);
+        long d1=formatter.parse(string1).getTime();
+        long d2=formatter.parse(string2).getTime();
+        return (d1-d2)/(1000*60*60*24);
+    }
+
+    public long getMsecDiff(Date date1, Date date2) {
+        long d1=date1.getTime();
+        long d2=date2.getTime();
+        return d1-d2;
+    }
+
+    public long getSecondDiff(Date date1, Date date2) {
+        long d1=date1.getTime();
+        long d2=date2.getTime();
+        return (d1-d2)/(1000);
+    }
+
+    public long getMinuteDiff(Date date1, Date date2) {
+        long d1=date1.getTime();
+        long d2=date2.getTime();
+        return (d1-d2)/(1000*60);
+    }
+
+    public long getHourDiff(Date date1, Date date2) {
+        long d1=date1.getTime();
+        long d2=date2.getTime();
+        return (d1-d2)/(1000*60*60);
+    }
+
+    public long getDayDiff(Date date1, Date date2) {
+        long d1=date1.getTime();
+        long d2=date2.getTime();
+        return (d1-d2)/(1000*60*60*24);
+    }
+}
