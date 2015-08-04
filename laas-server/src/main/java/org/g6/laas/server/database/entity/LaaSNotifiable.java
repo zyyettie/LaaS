9b0463352032c0d367441c00ab5820fff84caa95
Perry Wang <perrywangsh@gmail.com>
2015-08-04 14:15:34 +0800

    LaaSNotifiable class add default receiveFrom implementation

--- laas-server/src/main/java/org/g6/laas/server/database/entity/LaaSNotifiable.java
+++ laas-server/src/main/java/org/g6/laas/server/database/entity/LaaSNotifiable.java
@@ -10,14 +10,23 @@ import org.g6.laas.server.database.event.NotificationListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @MappedSuperclass
 @EntityListeners(NotificationListener.class)
-public abstract class LaaSNotifiable<U> extends LaaSAuditable<U> implements Notifiable<User> {
+public abstract class LaaSNotifiable<U> extends LaaSAuditable<U> implements Notifiable<U> {
     public LaaSNotifiable(Long id) {
         super(id);
     }
+
+    public U receiveFrom() {
+        return this.getCreatedBy();
+    }
+
+    public abstract Collection<U> sendTo();
+
+    public abstract String getSummary();
 }
