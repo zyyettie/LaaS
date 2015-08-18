d15c6bc856a067f277a2deca11334a42a7247f1a
Perry Wang <perrywangsh@gmail.com>
2015-08-18 17:39:47 +0800

    fix can't start laas-server

--- laas-server/src/main/java/org/g6/laas/server/database/repository/IFileRepository.java
+++ laas-server/src/main/java/org/g6/laas/server/database/repository/IFileRepository.java
@@ -2,6 +2,8 @@ package org.g6.laas.server.database.repository;
 
 import org.g6.laas.server.database.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IFileRepository extends JpaRepository<File, Long> {
 }
