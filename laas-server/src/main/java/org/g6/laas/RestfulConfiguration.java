74f6be1a11d893fe8c73fa7686a65e8357bea6d6
Perry Wang <perrywangsh@gmail.com>
2015-08-18 18:00:32 +0800

    expose all entity id field during development phase

--- laas-server/src/main/java/org/g6/laas/RestfulConfiguration.java
+++ laas-server/src/main/java/org/g6/laas/RestfulConfiguration.java
@@ -1,5 +1,10 @@
 package org.g6.laas;
 
+import org.g6.laas.server.database.entity.*;
+import org.g6.laas.server.database.entity.user.Notification;
+import org.g6.laas.server.database.entity.user.Role;
+import org.g6.laas.server.database.entity.user.User;
+import org.g6.laas.server.database.entity.user.Inbox;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
@@ -14,6 +19,8 @@ public class RestfulConfiguration extends RepositoryRestMvcConfiguration {
 
     @Override
     protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(User.class,Role.class, Notification.class,Inbox.class, Category.class, File.class, FileType.class,
+                            Product.class, Scenario.class,ScenarioHistory.class,Task.class, TaskHistory.class, Workflow.class);
         try {
             config.setReturnBodyOnCreate(true);
             config.setReturnBodyOnUpdate(true);
