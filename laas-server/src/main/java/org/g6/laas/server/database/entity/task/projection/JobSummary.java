72fa00b210b86180d6a869f897eeee229b1a235e
zhouanqing <zhouanqing@hotmail.com>
2015-09-09 11:05:34 +0800

    Add Scenario js and update demo data

---
+++ laas-server/src/main/java/org/g6/laas/server/database/entity/task/projection/JobSummary.java
@@ -0,0 +1,10 @@
+package org.g6.laas.server.database.entity.task.projection;
+
+import org.g6.laas.server.database.entity.Job;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "jobSummary", types = { Job.class })
+public interface JobSummary {
+    Long getId();
+    String getName();
+}
---
+++ laas-web/yo/app/scripts/modules/scenario.js
@@ -0,0 +1,57 @@
+LaaS.module('Scenario', function(Scenario, LaaS, Backbone, Marionette) {
+    'use strict';
+
+    var ScenarioView = Marionette.ItemView.extend({
+        initialize : function(options){
+            this.scenario = options.attributes;
+        },
+        template : function(data){
+            var template = JST['app/handlebars/scenario/detail'];
+            var html = template(data.scenario);
+            return html;
+        },
+        serializeData:function(){
+            return {scenario:this.scenario};
+        }
+    });
+
+    var ScenarioListView = Marionette.ItemView.extend({
+        initialize : function(options){
+            this.scenarios = options.scenarios;
+        },
+        template : function(data){
+            var template = JST['app/handlebars/scenario/list'];
+            var html = template(data);
+            return html;
+        },
+        serializeData:function(){
+            return {scenarios:this.scenarios};
+        }
+    });
+
+    var ScenarioController = Marionette.Controller.extend({
+        showScenarios: function() {
+            $.when(LaaS.request('scenario:entities')).done(function(data){
+               var view = new ScenarioListView(data);
+               LaaS.mainRegion.show(view);
+            });
+        },
+        showScenario: function(id){
+            $.when(LaaS.request('scenario:entity', {'id':id})).done(function(data){
+               var view = new ScenarioView(data);
+                LaaS.mainRegion.show(view);
+            });
+        }
+    });
+
+
+    LaaS.addInitializer(function() {
+        new Marionette.AppRouter({
+            appRoutes : {
+                'scenarios(/)': 'showScenarios',
+                'scenarios/:id(/)' : 'showScenario'
+            },
+            controller: new ScenarioController()
+        });
+    });
+});
