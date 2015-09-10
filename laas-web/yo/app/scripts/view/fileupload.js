3ef23d2acb0449f8c25c129da86d0e41bbbf1aa4
perrywang <perrywangsh@gmail.com>
2015-09-11 01:18:58 +0800

    defect fix for uploading function

--- laas-web/yo/app/scripts/view/fileupload.js
+++ laas-web/yo/app/scripts/view/fileupload.js
@@ -5,11 +5,10 @@ LaaS.module('Views', function (Views, LaaS, Backbone, Marionette) {
         onRender: function () {
             this.total = 0;
             var that = this;
-            this.$('#progress').progress();
             this.$('#progress').hide();
             this.$('#attaching').on('click', function () {
                 $('form').append('<input type="file" multiple="multiple" name="files[]" style="display:none;">');
-                $('input').on('change', function () {
+                $('input:last').on('change', function () {
                     console.log('selected files');
                     console.log(this.files);
                     var itemTempalte = JST['app/handlebars/fileupload/fileitem'];
@@ -21,25 +20,22 @@ LaaS.module('Views', function (Views, LaaS, Backbone, Marionette) {
                     }
 
                 });
-                $('input').last().click();
+                $('input:last').click();
             });
             var uploadingCallback = function (e) {
                 if (e.lengthComputable) {
                     var max = e.total;
                     var current = e.loaded;
-                    var Percentage = Math.round((current * 100) / max);
-                    $('#progress').progress({
-                        percent: Percentage, text: {
-                            active: Percentage + ' % Uploaded',
-                            success: 'Total ' + that.total + ' log files uploaded'
-                        }
-                    });
-                    if (Percentage >= 100) {
-                        $('#upload').toggleClass("disabled");
-                    }
+                    var Percentage = Math.round((current * 100) / max); 
+                    $('#progress').progress({percent:Percentage});
+                    $('#progress .label').text(''+ Percentage + ' % uploaded');              
                 }
             };
             this.$('#upload').click(function () {
+                $('#progress').removeClass('error success');    
+                $('#progress').addClass('active');
+                $('#progress').progress({percent:0});
+                $('#progress .label').text('uploading');
                 $('#progress').show();
                 $(this).toggleClass("disabled");
                 var formData = new FormData($('form')[0]);
@@ -55,11 +51,20 @@ LaaS.module('Views', function (Views, LaaS, Backbone, Marionette) {
                             uploadingxhr.upload.addEventListener('progress', uploadingCallback, false);
                         }
                         return uploadingxhr;
-                    },
-                    success: function (data) {
-                        console.log('succeed');
-                    }
-                });
+                    }                 
+                }).always(function(data,textStatus,error){
+                    $('#progress').removeClass('active');
+                    if(textStatus != 'success'){
+                            $('#progress').addClass('error');
+                            $('#progress').progress({percent:100});
+                            $('#progress .label').text('upload failed');
+        
+                        }else{
+                            $('#progress').progress({percent:100,text:{success:'total ' + that.total + ' uploaded' }});
+                        }
+                        $('#upload').removeClass('disabled');
+
+                });              
             });
 
         },
