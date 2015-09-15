53646f573c88c2fb1e66f67a20dbedfc409201ff
Perry Wang <perrywangsh@gmail.com>
2015-09-15 14:07:17 +0800

    implement handlebars view render

--- laas-core/src/main/java/org/g6/laas/core/engine/task/report/template/handlebars/HandlebarsReportView.java
+++ laas-core/src/main/java/org/g6/laas/core/engine/task/report/template/handlebars/HandlebarsReportView.java
@@ -6,7 +6,6 @@ import com.google.gson.Gson;
 import org.g6.laas.core.engine.task.report.ReportModel;
 import org.g6.laas.core.engine.task.report.template.TemplateReportView;
 import org.g6.laas.core.exception.ViewRenderException;
-import org.g6.laas.core.exception.ViewResolveException;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,12 +20,12 @@ public class HandlebarsReportView extends TemplateReportView {
     public String render(ReportModel model) {
 
         Gson json = new Gson();
-        String jsonModel =  json.toJson(model.asMap());
+        String jsonModel = json.toJson(model.asMap());
         Handlebars handlebars = new Handlebars();
-        try{
+        try {
             Template template = handlebars.compile(getTemplate().getCanonicalPath());
             return template.apply(jsonModel);
-        }catch(IOException ioe){
+        } catch (IOException ioe) {
             throw new ViewRenderException("can't apply model with template " + getTemplate().getName() + System.lineSeparator() + jsonModel);
         }
     }
