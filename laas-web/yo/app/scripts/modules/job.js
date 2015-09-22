9bf7247d5a81ffceb7a7fae14f1b8517c08ccbe7
zyyettie <zhangyuyuan1@gmail.com>
2015-09-22 20:57:35 +0800

    Be able to revoke backend JobController Successfully.

--- laas-web/yo/app/scripts/modules/job.js
+++ laas-web/yo/app/scripts/modules/job.js
@@ -9,8 +9,8 @@ LaaS.module('Job', function (Job, LaaS, Backbone, Marionette) {
             this.selected = options.selected;
             this.files = options.files;
         },
-        serializeData:function(){
-            var data = {job: this.job, scenarioList:this.scenarioList, fileList:this.fileList, selected:this.selected, files:this.files};
+        serializeData: function () {
+            var data = {job: this.job, scenarioList: this.scenarioList, fileList: this.fileList, selected: this.selected, files: this.files};
             if (this.job.id != undefined) {
                 var json = JSON.parse(data.job.parameters);
                 data.job.N = json["N"];
@@ -18,7 +18,7 @@ LaaS.module('Job', function (Job, LaaS, Backbone, Marionette) {
                 data.job.desc = json["order"] == "desc" ? "selected" : "";
                 data.job.asc = json["order"] == "asc" ? "selected" : "";
                 data.job.scenarioList = data.scenarioList;
-                for (var i=0; i<data.scenarioList.length; i++) {
+                for (var i = 0; i < data.scenarioList.length; i++) {
                     if (data.scenarioList[i].id == data.selected[0].id) {
                         data.job.scenarioList[i].selected = "selected";
                     }
@@ -31,11 +31,11 @@ LaaS.module('Job', function (Job, LaaS, Backbone, Marionette) {
         },
         onRender: function () {
             this.$('select').dropdown();
-            this.$('[name="selectedScenario"]').on('change',function(){
-                if(this.options[this.selectedIndex].innerHTML == 'Scenario - Top N'){
+            this.$('[name="selectedScenario"]').on('change', function () {
+                if (this.options[this.selectedIndex].innerHTML == 'Scenario - Top N') {
                     var render = JST['app/handlebars/job/topN'];
-                    $('#parameters').append(render({N:50,order:'desc', desc:'selected', asc:''}));
-                }else{
+                    $('#parameters').append(render({N: 50, order: 'desc', desc: 'selected', asc: ''}));
+                } else {
                     $('#parameters').empty();
                 }
             });
@@ -62,86 +62,103 @@ LaaS.module('Job', function (Job, LaaS, Backbone, Marionette) {
         saveJob: function () {
             var that = this;
             var json = Backbone.Syphon.serialize(this);
-            if(json.name == '' || json.scenario == ''){
+            if (json.name == '' || json.scenario == '') {
                 toastr.error('Please input name and select scenario.');
                 return;
             }
-            json.parameters = JSON.stringify({N:json['N'], order:json['order']});
+            json.parameters = JSON.stringify({N: json['N'], order: json['order']});
             //json.parameters = "{N:"+json['N']+", order:"+json['order']+"}";
             json.scenarios = [];
-            json.scenarios.push("/api/v1/scenarios/"+json.selectedScenario);
+            json.scenarios.push("/api/v1/scenarios/" + json.selectedScenario);
 
-            this.model.url='/jobs/'+json.id;
-            this.model.save(json,{patch:true,success:function(){
+            //this.model.url='/jobs/'+json.id;
+            this.model.save(json, {patch: true, success: function () {
                 toastr.info('Save Job successfully.');
                 LaaS.navigate('/jobs/' + that.model.id + '/edit');
                 $('#title').text('Editing Job');
 
-            },error:function(){
+            }, error: function () {
                 toastr.error('Save Job failed.');
             }});
         },
         runJob: function () {
             console.log("run the job");
-            $.when(LaaS.navigate("/controllers/jobs/"+this.job.id, {trigger:true})).done(function(response) {
-                var result = response;
-            });
+            var that = this;
+            var json = Backbone.Syphon.serialize(this);
+            if (json.name == '' || json.scenario == '') {
+                toastr.error('Please input name and select scenario.');
+                return;
+            }
+            json.parameters = JSON.stringify({N: json['N'], order: json['order']});
+            json.scenarios = [];
+            json.scenarios.push("/api/v1/scenarios/" + json.selectedScenario);
+            this.model.save(json, {patch: true, success: function (response) {
+                $.getJSON("/controllers/jobs/" + response.id).done(function (json) {
+                        toastr.info('Save and Run Job successfully.');
+                        LaaS.navigate('/jobs/' + json.id + '/edit');
+                    }).fail(function(json){
+                        toastr.info('Failed due to '+json);
+                    });
+
+            }, error: function () {
+                toastr.error('Save Job failed.');
+            }});
         },
         addFile: function () {
-            LaaS.navigate('/fileselect/'+this.job.id, {trigger:true});
+            LaaS.navigate('/fileselect/' + this.job.id, {trigger: true});
         }
     });
 
     var JobListView = Marionette.ItemView.extend({
-        initialize : function(options){
+        initialize: function (options) {
             this.jobs = options.jobs;
         },
-        template : function(data){
+        template: function (data) {
             var template = JST['app/handlebars/job/list'];
             var html = template(data);
             return html;
         },
-        serializeData:function(){
-            return {jobs:this.jobs};
+        serializeData: function () {
+            return {jobs: this.jobs};
         }
     });
 
     var JobController = Marionette.Controller.extend({
         jobnew: function () {
             $.when(LaaS.request('job:new'), LaaS.request('scenario:entities'), LaaS.request('file:entities'))
-                .done(function(job, scenario, file){
-                LaaS.mainRegion.show(new JobView({model:job, scenarioList:scenario.scenarios, fileList:file.files}));
-            });
+                .done(function (job, scenario, file) {
+                    LaaS.mainRegion.show(new JobView({model: job, scenarioList: scenario.scenarios, fileList: file.files}));
+                });
         },
         showJob: function (id) {
-            $.when(LaaS.request('job:entity', {'id':id}), LaaS.request('scenario:entities'), LaaS.request('file:entities'))
-                .done(function(job, scenario, file){
-                var scenarioModel = new LaaS.ScenarioModel();
-                scenarioModel.url = job.attributes._links.scenarios.href;
-                scenarioModel.fetch({
-                    success: function(model, response) {
-                        var selectedScenarios = response._embedded.scenarios;
-                        var fileModel = new LaaS.FileModel();
-                        fileModel.url = job.attributes._links.files.href;
-                        fileModel.fetch({
-                            success: function(model, reponse) {
-                                var view = new JobView({model:job, scenarioList:scenario.scenarios,
-                                    fileList:file.files, selected:selectedScenarios, files:reponse._embedded.files});
-                                LaaS.mainRegion.show(view);
-                            },
-                            error: function(err) {
-                                console.log(err);
-                            }
-                        })
-                    },
-                    error: function(err) {
-                        console.log(err);
-                    }
-                })
-            });
+            $.when(LaaS.request('job:entity', {'id': id}), LaaS.request('scenario:entities'), LaaS.request('file:entities'))
+                .done(function (job, scenario, file) {
+                    var scenarioModel = new LaaS.ScenarioModel();
+                    scenarioModel.url = job.attributes._links.scenarios.href;
+                    scenarioModel.fetch({
+                        success: function (model, response) {
+                            var selectedScenarios = response._embedded.scenarios;
+                            var fileModel = new LaaS.FileModel();
+                            fileModel.url = job.attributes._links.files.href;
+                            fileModel.fetch({
+                                success: function (model, reponse) {
+                                    var view = new JobView({model: job, scenarioList: scenario.scenarios,
+                                        fileList: file.files, selected: selectedScenarios, files: reponse._embedded.files});
+                                    LaaS.mainRegion.show(view);
+                                },
+                                error: function (err) {
+                                    console.log(err);
+                                }
+                            })
+                        },
+                        error: function (err) {
+                            console.log(err);
+                        }
+                    })
+                });
         },
-        showJobs: function() {
-            $.when(LaaS.request('job:entities')).done(function(data){
+        showJobs: function () {
+            $.when(LaaS.request('job:entities')).done(function (data) {
                 var view = new JobListView(data);
                 LaaS.mainRegion.show(view);
             });
