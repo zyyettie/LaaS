348fdc9d05cc1534ec370fd6008473a08a365f8c
zhouanqing <zhouanqing@hotmail.com>
2015-11-25 17:14:29 +0800

    Remove workflow repository and service

--- laas-server/src/main/java/org/g6/laas/server/database/repository/IWorkflowRepository.java
+++
@@ -1,9 +0,0 @@
-package org.g6.laas.server.database.repository;
-
-import org.g6.laas.server.database.entity.task.Workflow;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IWorkflowRepository extends JpaRepository<Workflow, Long> {
-}
