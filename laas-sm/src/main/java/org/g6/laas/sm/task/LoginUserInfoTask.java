20af2b597d0d4a35bfe66c15db77bfa515ad8667
Johnson Jiang <lizwjiang@gmail.com>
2015-12-01 00:50:21 +0800

    Implement Login User Task with workflow component.

--- laas-sm/src/main/java/org/g6/laas/sm/task/LoginUserInfoTask.java
+++ laas-sm/src/main/java/org/g6/laas/sm/task/LoginUserInfoTask.java
@@ -1,13 +1,22 @@
 package org.g6.laas.sm.task;
 
+import org.g6.laas.core.engine.task.ChainTask;
+import org.g6.laas.core.engine.task.TaskChain;
 import org.g6.laas.sm.log.login.Info;
+import org.g6.laas.sm.vo.LoginInfo;
 
 import java.util.List;
+import java.util.Map;
 
-public class LoginUserInfoTask extends SMRTETask<List<Info>>{
-    private String user;
+public class LoginUserInfoTask extends ChainTask<LoginInfo> {
+
     @Override
-    protected List<Info> process() {
+    protected LoginInfo process() {
         return null;
+    }
+
+    @Override
+    public void doTask(Map request, Map response, TaskChain chain) {
+         LoginInfo info = process();
     }
 }
---
+++ laas-sm/src/main/java/org/g6/laas/sm/vo/LoginInfo.java
@@ -0,0 +1,9 @@
+package org.g6.laas.sm.vo;
+
+import lombok.Data;
+
+@Data
+public class LoginInfo {
+    private String licenseType;
+    private String clientType;
+}
