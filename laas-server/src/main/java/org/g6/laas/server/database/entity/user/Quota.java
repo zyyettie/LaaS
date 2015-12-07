00228986baa7d96d8157a0aa124a1f4af860c2ef
zhouanqing <zhouanqing@hotmail.com>
2015-12-07 17:27:01 +0800

    Update quota entity

--- laas-server/src/main/java/org/g6/laas/server/database/entity/user/Quota.java
+++ laas-server/src/main/java/org/g6/laas/server/database/entity/user/Quota.java
@@ -1,6 +1,7 @@
 package org.g6.laas.server.database.entity.user;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.g6.laas.server.database.entity.LaaSPersistable;
 
@@ -12,6 +13,7 @@ import javax.persistence.Table;
 @Data
 @Entity
 @Table
+@NoArgsConstructor
 public class Quota extends LaaSPersistable {
     private static final long serialVersionUID = -7002369595907189117L;
 
@@ -21,7 +23,7 @@ public class Quota extends LaaSPersistable {
 
     private long maxFileSize;
 
-    private long SpaceQuota;
+    private long spaceQuota;
 
     private long usedSpace;
 }
