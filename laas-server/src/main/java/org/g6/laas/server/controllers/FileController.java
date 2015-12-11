5ad829f9fa437c0d9db08a0543d2d58af4e6f8c1
zhouanqing <zhouanqing@hotmail.com>
2015-12-11 13:34:36 +0800

    When delete file, release its used space in quota

--- laas-server/src/main/java/org/g6/laas/server/controllers/FileController.java
+++ laas-server/src/main/java/org/g6/laas/server/controllers/FileController.java
@@ -1,7 +1,9 @@
 package org.g6.laas.server.controllers;
 
 import org.g6.laas.server.database.entity.file.File;
+import org.g6.laas.server.database.entity.user.Quota;
 import org.g6.laas.server.database.repository.IFileRepository;
+import org.g6.laas.server.database.repository.IQuotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +19,21 @@ public class FileController {
     @Autowired
     private IFileRepository fileRep;
 
+    @Autowired
+    private IQuotaRepository quotaRep;
+
     @RequestMapping(value = "/controllers/files", method = RequestMethod.POST)
     ResponseEntity<String> deleteFiles(@RequestBody List<File> files){
         for(File file : files){
             File tempFile = fileRep.findOne(file.getId());
             tempFile.setIsRemoved("Y");
-            fileRep.save(tempFile);
+            tempFile = fileRep.save(tempFile);
+            Quota quota = tempFile.getCreatedBy().getQuota();
+            long fileSize = tempFile.getSize() == null ? 0L : tempFile.getSize();
+            long usedSpace = quota.getUsedSpace() - fileSize ;
+            usedSpace = usedSpace > 0 ? usedSpace : 0L;
+            quota.setUsedSpace(usedSpace);
+            quotaRep.save(quota);
         }
 
         return new ResponseEntity("{\"SUCCESS\":TRUE}", HttpStatus.OK);
