e1e689230b6d633345b1ba9b22148576650e388e
Johnson Jiang <lizwjiang@gmail.com>
2015-12-22 01:26:46 +0800

    add role and quota for user registration

--- laas-server/src/main/java/org/g6/laas/server/controllers/UserController.java
+++ laas-server/src/main/java/org/g6/laas/server/controllers/UserController.java
@@ -1,6 +1,9 @@
 package org.g6.laas.server.controllers;
 
+import org.g6.laas.server.database.entity.user.Quota;
+import org.g6.laas.server.database.entity.user.Role;
 import org.g6.laas.server.database.entity.user.User;
+import org.g6.laas.server.database.repository.IRoleRepository;
 import org.g6.laas.server.database.repository.IUserRepository;
 import org.g6.laas.server.database.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     IUserRepository userRepository;
+    @Autowired
+    IRoleRepository roleRepository;
+
     @RequestMapping(value = "/controllers/users")
-    ResponseEntity<String> saveUser(@RequestBody User request){
-        User existingUser = userRepository.findByName(request.getName());
-        if(existingUser != null){
-            return new ResponseEntity("{\"name\":\""+request.getName()+"\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+    ResponseEntity<String> saveUser(@RequestBody User user) {
+        User existingUser = userRepository.findByName(user.getName());
+        if (existingUser != null) {
+            return new ResponseEntity("{\"name\":\"" + user.getName() + "\"}", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        User u = userRepository.save(request);
-        return new ResponseEntity("{\"name\":\""+u.getName()+"\"}", HttpStatus.OK);
+        Role role = roleRepository.findOne(2l);
+        user.setRole(role);
+
+        Quota quota = new Quota();
+        quota.setUsedSpace(524288000);
+        user.setQuota(quota);
+
+        User u = userRepository.save(user);
+
+        return new ResponseEntity("{\"name\":\"" + u.getName() + "\"}", HttpStatus.OK);
     }
 }
