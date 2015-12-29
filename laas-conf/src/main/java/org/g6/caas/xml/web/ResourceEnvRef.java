54e3dd4b9acca259f12ee627e4d9cc1ef61ca4f6
Johnson Jiang <lizwjiang@gmail.com>
2015-12-29 09:58:51 +0800

    remove comments on class

--- laas-conf/src/main/java/org/g6/caas/xml/web/ResourceEnvRef.java
+++ laas-conf/src/main/java/org/g6/caas/xml/web/ResourceEnvRef.java
@@ -1,11 +1,7 @@
 package org.g6.caas.xml.web;
 
-/**
- * Created with IntelliJ IDEA.
- * User: jili
- * Date: 7/23/15
- * Time: 1:32 PM
- * To change this template use File | Settings | File Templates.
- */
+import lombok.Data;
+
+@Data
 public class ResourceEnvRef {
 }
